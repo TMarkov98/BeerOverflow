0941e4b6ea92c4587479c911f357ea4c57bd4f46
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-04-09 12:39:51 +0300

    Added BreweryAPIController and CountryAPIController

---
+++ BeerOverflow/BeerOverflow/API_Controllers/CountryAPIController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerOverflow.Web.API_Controllers
+{
+    [Route("api/countries")]
+    [ApiController]
+    public class CountryAPIController : ControllerBase
+    {
+    }
+}
