dac6d4b1f5a976d92d8660748f85fb55d5f9b877
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-04-11 19:02:48 +0300

    Updated UserRole

---
+++ BeerOverflow/BeerOverflow.Models/Contracts/IUserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Models.Contracts
+{
+    interface IUserRole
+    {
+        int Id { get; set; }
+        string RoleName { get; set; }
+    }
+}
---
+++ BeerOverflow/BeerOverflow.Models/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using BeerOverflow.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeerOverflow.Models
+{
+    public class UserRole : IUserRole
+    {
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+    }
+}
