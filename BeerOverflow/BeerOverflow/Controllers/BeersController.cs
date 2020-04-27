1822c84b4e612f8e56c6b566f36ecbcfd12ca47d
xhism.kml@gmail.com <xhism.kml@gmail.com>
2020-04-27 18:26:32 +0300

    Fixed Build Error.

--- BeerOverflow/BeerOverflow/Controllers/BeersController.cs
+++ BeerOverflow/BeerOverflow/Controllers/BeersController.cs
@@ -17,7 +17,6 @@ namespace BeerOverflow.Web.Controllers
         {
             _context = context;
         }
-        public PaginatedList<Beer> Beers { get; set; }
 
         // GET: Beers
         public async Task<IActionResult> Index(string searchString)
